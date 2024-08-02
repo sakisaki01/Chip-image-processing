15795ba55c832295ae98c0b784c842f47bae2e47
srn <Songruinan01@Gmail.com>
2024-08-02 16:03:40 +0800

    转换为mp4视频格式

---
+++ input_video.py
@@ -0,0 +1,33 @@
+import cv2
+import os
+
+input_folder = '240723-0724/input-5_cut'
+output_video_path = '240723-0724/input-5_cut.mp4'   #后面的 .mp4 不能删去
+fps = 30  # 视频帧率
+
+
+def images_to_video(input_folder, output_video_path, fps):
+    # 获取输入文件夹中所有图片的文件名，并按照文件名排序
+    image_files = sorted([os.path.join(input_folder, file) for file in os.listdir(input_folder) if
+                          file.endswith(('.jpg', '.png', '.jpeg'))])
+
+    # 读取第一张图片，获取图片尺寸
+    first_image = cv2.imread(image_files[0])
+    height, width, _ = first_image.shape
+
+    # 创建视频编写器
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 可以根据需要更改编码器
+    video_writer = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+
+    # 逐帧写入视频
+    for image_file in image_files:
+        image = cv2.imread(image_file)
+        video_writer.write(image)
+
+    # 释放资源
+    video_writer.release()
+    print("将文件夹：" + input_folder + "导出为MP4格式，地址为：" + output_video_path)
+
+
+# 调用函数将图片合成为视频
+images_to_video(input_folder, output_video_path, fps)
